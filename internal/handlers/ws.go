008c1b72d9a49b325417329713560d6eae0245f0
Rick Boss <rickeboss@gmail.com>
2020-10-02 17:05:00 -0700

    Improve logging, close connections and handle upgrade error

--- internal/handlers/ws.go
+++ internal/handlers/ws.go
@@ -19,30 +19,37 @@ func (handler *WebSocketHandler) ServeHTTP(w http.ResponseWriter, req *http.Requ
 	conn, err := handler.Upgrader.Upgrade(w, req, nil)
 	if err != nil {
 		handler.Logger.Println("Error upgrading connection:", err)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+	defer func() {
+		// ensure connection is closed by forcibly closing it
+		if err := conn.Close(); err != nil {
+			handler.Logger.Println("Error closing connection:", err)
+		}
+	}()
 
 	err = conn.WriteMessage(websocket.TextMessage,
 		[]byte(fmt.Sprintf("[%s] hello :) type a message and press ENTER", handler.ID)),
 	)
 
 	if err != nil {
-		log.Println(err)
+		handler.Logger.Println("Error writing greeting to socket:", err)
 		// continue on regardless of error
 	}
 
 	for {
 		messageType, p, err := conn.ReadMessage()
 		if err != nil {
-			log.Println(err)
+			handler.Logger.Println("Error reading message:", err)
 			return
 		}
-		fmt.Println("Got message:", strings.Trim(string(p), "\n"))
+		handler.Logger.Println("Got message:", strings.Trim(string(p), "\n"))
 		err = conn.WriteMessage(messageType,
 			append([]byte(fmt.Sprintf("[%s] ", handler.ID)), p...),
 		)
 		if err != nil {
-			log.Println(err)
+			handler.Logger.Println("Error writing message to socket:", err)
 			return
 		}
 	}
